66c4771a6fcbbdfd70b903e1d73fbd4f09b26ef3
Nick Capurso <capurso.nick@gmail.com>
2018-09-10 22:11:00 -0400

    Add simple login screen

--- app/src/main/java/com/gwu/android/androidtweets/LoginActivity.kt
+++ app/src/main/java/com/gwu/android/androidtweets/LoginActivity.kt
@@ -2,11 +2,47 @@ package com.gwu.android.androidtweets
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var usernameEditText: EditText
+
+    private lateinit var passwordEditText: EditText
+
+    private lateinit var loginButton: Button
+
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        usernameEditText = findViewById(R.id.username)
+        passwordEditText = findViewById(R.id.password)
+        loginButton = findViewById(R.id.login)
+        progressBar = findViewById(R.id.progressBar)
+
+        usernameEditText.addTextChangedListener(textWatcher)
+        passwordEditText.addTextChangedListener(textWatcher)
+
+        loginButton.setOnClickListener { progressBar.visibility = View.VISIBLE }
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable) {}
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            val usernameText: String = usernameEditText.text.toString()
+            val passwordText: String = passwordEditText.text.toString()
+            loginButton.isEnabled = usernameText.isNotEmpty() && passwordText.isNotEmpty()
+        }
     }
 }
---
+++ app/src/main/java/com/gwu/android/androidtweets/LoginActivityJava.java
@@ -0,0 +1,58 @@
+package com.gwu.android.androidtweets;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+public class LoginActivityJava extends AppCompatActivity {
+
+    private EditText usernameEditText;
+
+    private EditText passwordEditText;
+
+    private Button loginButton;
+
+    private ProgressBar progressBar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+        progressBar = findViewById(R.id.progressBar);
+
+        usernameEditText.addTextChangedListener(textWatcher);
+        passwordEditText.addTextChangedListener(textWatcher);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String usernameText = usernameEditText.getText().toString();
+            String passwordText = passwordEditText.getText().toString();
+            loginButton.setEnabled(!usernameText.trim().isEmpty() && !passwordText.trim().isEmpty());
+        }
+    };
+}
